4349f2fac6c0a1559c7fc0c35aa8ba5ab7b27cc2
luqmanshaban <luqmanforeal@gmail.com>
2023-01-25 09:35:56 +0300

    carousel

--- index.js
+++ index.js
@@ -18,3 +18,20 @@ document.querySelectorAll('.navLink').forEach(n => n.addEventListener('click', (
     navMenu.classList.remove('active');
 
 }))
+
+// HEADER 
+
+const images = ['./images/bg11.jpg','./images/bg2.jpg','./images/bg3.jpg'];
+const interval = setInterval( () => {
+    startCarousel();
+}, 3000)
+
+let index = 1;
+
+startCarousel = () => {
+    header.style.backgroundImage = `url(${images[index++]})`;
+    header.classList.remove('fade');
+    void header.offsetWidth;
+    header.classList.add('fade');
+    if(index > images.length -1) index = 0;
+}
